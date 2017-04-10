971c5a75ea5063d13dab8afc2ef6fb550155328e
colopreda <apredazzi@gmail.com>
2017-04-09 23:10:25 -0300

    Added drawer and toolbar to BaseActivity so now all the activities have them

--- app/src/main/java/com/fiuba/apredazzi/tp_taller2_android/activities/ProfileActivity.java
+++ app/src/main/java/com/fiuba/apredazzi/tp_taller2_android/activities/ProfileActivity.java
@@ -1,15 +1,26 @@
 package com.fiuba.apredazzi.tp_taller2_android.activities;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+import com.fiuba.apredazzi.tp_taller2_android.BaseActivity;
 import com.fiuba.apredazzi.tp_taller2_android.R;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        //inflate your activity layout here!
+        View contentView = inflater.inflate(R.layout.activity_profile, null, false);
+        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.content_frame);
+        frameLayout.addView(contentView);
     }
 }
--- app/src/main/java/com/fiuba/apredazzi/tp_taller2_android/activities/SongsListActivity.java
+++ app/src/main/java/com/fiuba/apredazzi/tp_taller2_android/activities/SongsListActivity.java
@@ -1,12 +1,16 @@
 package com.fiuba.apredazzi.tp_taller2_android.activities;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+import com.fiuba.apredazzi.tp_taller2_android.BaseActivity;
 import com.fiuba.apredazzi.tp_taller2_android.R;
 import com.fiuba.apredazzi.tp_taller2_android.adapter.SongAdapter;
 import com.fiuba.apredazzi.tp_taller2_android.interfaces.RecyclerViewClickListener;
@@ -14,7 +18,7 @@ import com.fiuba.apredazzi.tp_taller2_android.model.Song;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SongsListActivity extends AppCompatActivity implements RecyclerViewClickListener {
+public class SongsListActivity extends BaseActivity implements RecyclerViewClickListener {
 
     private List<Song> songsList;
     private RecyclerView mRecyclerView;
@@ -24,7 +28,12 @@ public class SongsListActivity extends AppCompatActivity implements RecyclerView
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_songs_list);
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        //inflate your activity layout here!
+        View contentView = inflater.inflate(R.layout.activity_songs_list, null, false);
+        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.content_frame);
+        frameLayout.addView(contentView);
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
