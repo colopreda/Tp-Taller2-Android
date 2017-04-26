f5e710372cffd3abdeb302b06b0f1c92271748ea
colopreda <apredazzi@gmail.com>
2017-04-26 20:16:50 -0300

    The song is streamed using an url

--- app/src/main/java/com/fiuba/apredazzi/tp_taller2_android/activities/SongActivity.java
+++ app/src/main/java/com/fiuba/apredazzi/tp_taller2_android/activities/SongActivity.java
@@ -1,21 +1,44 @@
 package com.fiuba.apredazzi.tp_taller2_android.activities;
 
 import android.app.Activity;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 
 import androgeek.material.library.MaterialMusicPlayerView;
 import com.fiuba.apredazzi.tp_taller2_android.BaseActivity;
 import com.fiuba.apredazzi.tp_taller2_android.R;
+import java.io.IOException;
 
 public class SongActivity extends BaseActivity {
 
     MaterialMusicPlayerView mpv;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song);
+
+//        mediaPlayer = MediaPlayer.create(this, R.raw.soy);
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(final MediaPlayer mp) {
+                mpv.setMax(mediaPlayer.getDuration() / 1000);
+                mpv.start();
+                mediaPlayer.start();
+            }
+        });
+
+        try {
+            mediaPlayer.setDataSource("http://slider.kz/download/167/cs8-3v4.vk-cdn.net/p10/ae6c2eeac0573d/Ed%20Sheeran%20-%20Galway%20Girl.mp3");
+            mediaPlayer.prepareAsync();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         mpv = (MaterialMusicPlayerView) findViewById(R.id.mpv);
         mpv.setCoverURL("http://i.ytimg.com/vi/y7HvUEPjbzU/hqdefault.jpg");
@@ -23,12 +46,28 @@ public class SongActivity extends BaseActivity {
         mpv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mpv.isRotating())
+                if (mpv.isRotating()) {
                     mpv.stop();
-                else
+                    mediaPlayer.pause();
+                }
+                else {
                     mpv.start();
+                    mediaPlayer.start();
+                }
             }
         });
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (mediaPlayer != null) {
+            if (mediaPlayer.isPlaying()) {
+                mediaPlayer.stop();
+            }
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
 }
